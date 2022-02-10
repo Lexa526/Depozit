8fff95a720c77413b7115f9d2b4f811f21338190
Lexa <86785362+Lexa526@users.noreply.github.com>
2022-02-10 18:24:04 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -6,3 +6,12 @@ let n = document.querySelector(".n").value
 let fv = pv * (1 + ((r*n)/100))
 alert (fv)
 })
+let aButton = document.querySelector(".result1")
+pButton.addEventListener("click", (e) => {
+let r = document.querySelector(".r1").value
+let pv = document.querySelector(".pv1").value
+let n = document.querySelector(".n1").value
+let m = document.querySelector(".m").value
+let fv = pv * ((1 + ((r/m)/100)) ** (n *m))
+alert (fv)
+})
